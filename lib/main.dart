843752522df0ea32534d83442aec8798e78fe29b
SahanRashmika613 <rashmikaliyanage2001@gmail.com>
2025-02-14 11:15:59 +0530

    edited registerpage.dart connecting main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:namer_app/Presentation/Price_analysis.dart';
 import 'package:namer_app/Presentation/first_screen/Login.dart';
 import 'package:namer_app/Presentation/first_screen/first_screen.dart';
+import 'package:namer_app/Presentation/registerpage/registerpage.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,6 @@ void main() {
 class MyApp extends StatefulWidget {
   @override
   State<MyApp> createState() => _MyAppState();
-  
 }
 
 class _MyAppState extends State<MyApp> {
@@ -19,8 +19,9 @@ class _MyAppState extends State<MyApp> {
   double _isVisible = 0.0;
   String _nameToChange = "flow";
   String _inputText = "";
-  int _hours=0;
-  final ScrollController _scrollController = ScrollController(); // Step 1: Declare ScrollController
+  int _hours = 0;
+  final ScrollController _scrollController =
+      ScrollController(); // Step 1: Declare ScrollController
 
   void numbers() {
     setState(() {
@@ -30,7 +31,8 @@ class _MyAppState extends State<MyApp> {
 
   void changeColor() {
     setState(() {
-      _scaffoldColor = _scaffoldColor == Colors.black12 ? Colors.blue : Colors.black12;
+      _scaffoldColor =
+          _scaffoldColor == Colors.black12 ? Colors.blue : Colors.black12;
     });
   }
 
@@ -45,25 +47,22 @@ class _MyAppState extends State<MyApp> {
       _nameToChange = _nameToChange == "flow" ? "rukaass" : "flow";
     });
   }
-  void changeHour(){
+
+  void changeHour() {
     setState(() {
       _hours++;
-      
-      
     });
   }
 
   @override
   void dispose() {
-    _scrollController.dispose(); // Dispose the controller to prevent memory leaks
+    _scrollController
+        .dispose(); // Dispose the controller to prevent memory leaks
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Login()
-      );
-    
+    return MaterialApp(home: Login());
   }
 }
