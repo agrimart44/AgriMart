3eee43059248c22962f6f75b240aad3255ff8482
chamudithaweeramanthri <chamudithaweeramanthri@gmail.com>
2025-02-15 22:35:15 +0530

    Add Background image
    background image added to the personal-info page.

---
+++ Frontend/lib/Settings/personal Info/personal_info.dart
@@ -0,0 +1,38 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children:[
+          //background
+          Positioned.fill(child:Image.asset('lib/assets/first_page_background.jpg',fit: BoxFit.cover,))
+        ],
+      ),
+    );
+  }
+}
--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -10,7 +10,6 @@ void main() {
 class MyApp extends StatefulWidget {
   @override
   State<MyApp> createState() => _MyAppState();
-  
 }
 
 class _MyAppState extends State<MyApp> {
