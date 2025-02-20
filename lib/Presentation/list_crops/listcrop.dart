79182ad3cfbbf9344fdaee5656234e2ea136ad0c
SahanRashmika613 <rashmikaliyanage2001@gmail.com>
2025-02-20 19:17:08 +0530

    update ListCropScreenState class

--- lib/Presentation/list_crops/listcrop.dart
+++ lib/Presentation/list_crops/listcrop.dart
@@ -8,6 +8,27 @@ class ListCropScreen extends StatefulWidget {
 }
 
 class _ListCropScreenState extends State<ListCropScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final List<String> _photos = <String>[];
+  final TextEditingController _harvestDataController = TextEditingController();
+  final TextEditingController _priceController = TextEditingController();
+  final TextEditingController _locationController = TextEditingController();
+  final TextEditingController _quantityController = TextEditingController();
+
+  Future<void> _selectData(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2100));
+    if (picked != null) {
+      setState(() {
+        _harvestDataController.text =
+            "${picked.day}/${picked.month}/${picked.year}";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
