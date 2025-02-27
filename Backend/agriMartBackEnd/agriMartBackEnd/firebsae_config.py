c41d2e490f6453f076435d96bee9483e133caa26
Danutha Imbulpitiya <danuthaimbulpitiya@gmail.com>
2025-02-27 19:39:44 +0530

    Updated firebase_config.py

--- Backend/agriMartBackEnd/agriMartBackEnd/firebsae_config.py
+++ Backend/agriMartBackEnd/agriMartBackEnd/firebsae_config.py
@@ -1,12 +1,12 @@
 import firebase_admin
+from firebase_admin import credentials, firestore
 from firebase_admin import credentials, firestore, auth
 
 
 # Initialize Firebase with the service account key
 cred = credentials.Certificate('firebase/agri-mart-add65-firebase-adminsdk-fbsvc-b8bb3bc221.json')  # Replace with actual path
-firebase_admin.initialize_app(cred, {
-    'storageBucket': 'your-app-id.appspot.com'  # Specify your Firebase storage bucket
-})
+firebase_admin.initialize_app(cred)
+
 # Initialize Firestore
 db = firestore.client()
 
@@ -17,6 +17,3 @@ def verify_firebase_token(token):
         return decoded_token
     except Exception as e:
         return None
-    
-
-    
