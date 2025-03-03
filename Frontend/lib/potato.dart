f5890eab5f317e2751c6a67ee9f960568fa26585
chamudithaweeramanthri <chamudithaweeramanthri@gmail.com>
2025-03-03 18:43:39 +0530

    basic structure of potato.dart file

--- Frontend/lib/potato.dart
+++ Frontend/lib/potato.dart
@@ -1,3 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: TextTheme(
+          // Add arimo font
+          bodyLarge: GoogleFonts.arimo(
+            textStyle: const TextStyle(fontSize: 24),
+            fontWeight: FontWeight.bold,
+          ),
+          bodyMedium: GoogleFonts.arimo(
+            textStyle: const TextStyle(fontSize: 18),
+          ),
+        ),
+      ),
+      home: const MyHomePage(title: 'Home'), // Set Home Widget
+    );
+  }
+}
