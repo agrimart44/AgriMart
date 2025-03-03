cc947bf62e8f01044d5914e9c1f4f502265dc040
Gokula Nandhan <gokula.20233027@iit.ac.lk>
2025-03-03 14:32:14 +0530

    update mobile number page
    created the mobile number page for the authentication

---
+++ Frontend/lib/Security/Authentication/mobile_number/update_mobile_number_page.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+void main() {
+    runApp(const ChangePasswordScreen());
+}
+
+class ChangePasswordScreen extends StatelessWidget {
+  const ChangePasswordScreen({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+                debugShowCheckedModeBanner: false,
+                home: Scaffold(
+                body: Stack(
+                children: [
+        Container(
+                decoration: const BoxDecoration(
+                image: DecorationImage(
+                image: AssetImage("assets/background.jpg"), // Background image
+                fit: BoxFit.cover,
+                ),
+              ),
+            ),
+        SafeArea(
+                child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+        // Back button and menu icon
+        Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+        IconButton(
+                icon: const Icon(Icons.arrow_back, color: Colors.white),
+        onPressed: () {},
+        style: IconButton.styleFrom(
+                backgroundColor: Colors.green,
+                shape: const CircleBorder(),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+
+        // Title and description
+                    const Text(
+                "Update your mobile number",
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text(
+                "Add a mobile phone  number so you can always "
+                "get back into your account. It also helps us notify you.",
+                style: TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
+                    const SizedBox(height: 20),
+
+        // Password Input Fields
+        buildPasswordField("Enter mobile number"),
+
+
+
+                    const SizedBox(height: 500),
+
+        Align(
+          alignment: Alignment.center,
+          // Change Password Button
+        child:SizedBox(
+                width: 200,
+                height: 50,
+                child: ElevatedButton(
+                onPressed: () {},
+        style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25),
+                          ),
+                        ),
+        child: const Text(
+                "Next",
+                style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                      ),
+                    ),
+        )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+    }
+
+    Widget buildPasswordField(String hint) {
+        return TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                hintText: hint,
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: BorderSide.none,
+        ),
+      ),
+    );
+    }
+}
