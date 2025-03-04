5f3d209826a570916f6b8cff9c13ce9efb933b81
Danutha Imbulpitiya <danuthaimbulpitiya@gmail.com>
2025-03-04 15:19:04 +0530

    Updated Private key

--- Backend/agriMartBackEnd/agriMartBackEnd/firebsae_config.py
+++ Backend/agriMartBackEnd/agriMartBackEnd/firebsae_config.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Initialize Firebase with the service account key
-cred = credentials.Certificate('firebase/agri-mart-add65-firebase-adminsdk-fbsvc-b8bb3bc221.json')  # Replace with actual path
+cred = credentials.Certificate('firebase/agri-mart-add65-firebase-adminsdk-fbsvc-a93f240f48.json')  # Replace with actual path
 firebase_admin.initialize_app(cred)
 
 # Initialize Firestore
