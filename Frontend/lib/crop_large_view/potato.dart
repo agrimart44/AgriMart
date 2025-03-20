b35327834055ac7a02168b28d33856df54156e01
chamudithaweeramanthri <chamudithaweeramanthri@gmail.com>
2025-03-20 12:31:59 +0530

    language files are updated for potato.dart

--- Frontend/lib/crop_large_view/potato.dart
+++ Frontend/lib/crop_large_view/potato.dart
@@ -10,6 +10,7 @@ import 'package:google_fonts/google_fonts.dart';
 
 //import localization
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 class CropLargeView extends StatefulWidget {
   final Crop crop;
 
