1cabd0f4bae45be740c0e895ea8cbf5d31f09128
Danutha Imbulpitiya <danuthaimbulpitiya@gmail.com>
2025-03-21 15:30:41 +0530

    Main Branch updated with Frontend and Backend

--- Backend/agriMartBackEnd/agriMartBackEnd/firebsae_config.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-# Initialize Firebase with the service account key
-cred = credentials.Certificate('firebase/agri-mart-add65-firebase-adminsdk-fbsvc-a93f240f48.json')  # Replace with actual path
-firebase_admin.initialize_app(cred)
-
-# Initialize Firestore
-db = firestore.client()
