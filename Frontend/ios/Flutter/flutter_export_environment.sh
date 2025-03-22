403801cf3a6b00a9d940d111862eee0dcb53ca41
chamudithaweeramanthri <chamudithaweeramanthri@gmail.com>
2025-03-22 13:05:38 +0530

    logo updated
    add agrimart logo to the application.

--- Frontend/ios/Flutter/flutter_export_environment.sh
+++ Frontend/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\rukshan\Downloads\flutter_windows_3.29.0-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\rukshan\Pictures\Cloneeeeee\AgriMart\Frontend"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\This PC\Documents\tomatoView\AgriMart\Frontend"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
