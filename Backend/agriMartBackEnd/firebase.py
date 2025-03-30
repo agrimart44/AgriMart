f8a90ea34016ef62cf085e7cca77b0246404c35b
Danutha Imbulpitiya <danuthaimbulpitiya@gmail.com>
2025-03-30 17:05:16 +0530

    Permission set up

--- Backend/agriMartBackEnd/firebase.py
+++ Backend/agriMartBackEnd/firebase.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, auth, firestore
 
 # Initialize Firebase with the service account key
-cred = credentials.Certificate('firebase/agri-mart-add65-firebase-adminsdk-fbsvc-a93f240f48.json')  # Replace with actual path
+cred = credentials.Certificate('firebase/agri-mart-add65-firebase-adminsdk-fbsvc-a93f240f48.json') 
 firebase_admin.initialize_app(cred)
 
 # Initialize Firestore
