958c680f5798c86064d6f0fe3a7c8e06f1a6dd39
Rukiya <125763362+Rukiya203@users.noreply.github.com>
2025-03-30 09:21:03 +0530

    New pdfs added

--- Frontend/test/widget_test.dart
+++ Frontend/test/widget_test.dart
@@ -1,28 +1,29 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:namer_app/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:namer_app/Presentation/first_screen/first_screen.dart';
+import 'package:namer_app/main.dart';
 
-// void main() {
-//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//     // Build the app and trigger a frame.
-//     await tester.pumpWidget(const MyApp());
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build the app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
 
-//     // Verify that the FirstScreen is displayed
-//     expect(find.byType(FirstScreen), findsOneWidget);
+    // Verify that the FirstScreen is displayed
+    expect(find.byType(FirstScreen), findsOneWidget);
 
-//     // Verify that the app bar title is correct
-//     expect(find.text('AgriMart'), findsOneWidget);
+    // Verify that the app bar title is correct
+    expect(find.text('AgriMart'), findsOneWidget);
 
-//     // Verify that the counter starts at 0
-//     expect(find.text('0'), findsOneWidget);
-//     expect(find.text('1'), findsNothing);
+    // Verify that the counter starts at 0
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
 
-//     // Tap the '+' icon and trigger a frame.
-//     await tester.tap(find.byIcon(Icons.add));
-//     await tester.pump();
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
 
-//     // Verify that the counter has incremented to 1
-//     expect(find.text('0'), findsNothing);
-//     expect(find.text('1'), findsOneWidget);
-//   });
-// }
+    // Verify that the counter has incremented to 1
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+  });
+}
